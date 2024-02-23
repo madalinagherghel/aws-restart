15685b67070bb9230c46f1812b2a28f491e2d0bf
Madalina Gherghel <madalina.gherghel@globallogic.com>
2024-02-23 18:18:21 +0200

    some labs, work in progress

---
+++ number.py
@@ -0,0 +1,20 @@
+print("Python has three numeric types : int, float and complex")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print("---------exercise 4-----------")
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print("------exercise 5 BOOLEAN-----")
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=False
